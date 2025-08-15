32ed558b485f03f12d403b496aaa2b3492e6a749
Azri Kuncoro Jati <geozray@gmail.com>
2025-08-15 16:21:41 +0700

    add update tugas project mini coding

---
+++ js/script.js
@@ -0,0 +1,20 @@
+console.log("Hello World!");
+
+// welcomeSpeech();
+
+// call the function to welcome speech
+function welcomeSpeech() {
+    let username = prompt("What's your name?");
+    if (username != '') {
+        document.getElementById("user-greeting").textContent = username;
+    }
+}
+
+function sendMessage() {
+    let message = document.getElementById("user-message").value;
+    if (message != '') {
+        alert("Message sent: " + message);
+    } else {
+        alert("Please enter a message before sending.");
+    }
+}
