a4362374aeb190120bbc97a6724ac9b58447d330
Azri Kuncoro Jati <geozray@gmail.com>
2025-08-16 11:29:19 +0700

    refactor form inputs for responsive design and improve message alert functionality

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
+// welcomeSpeech();
 console.log("Hello World!");
 
-// welcomeSpeech();
-
-// call the function to welcome speech();
+// call the function to welcome speech
 function welcomeSpeech() {
     let username = prompt("What's your name?");
     if (username != '') {
@@ -10,32 +9,13 @@ function welcomeSpeech() {
     }
 }
 
+// Function to send message
 function sendMessage() {
-    let message = document.getElementById("user-message").value};
+    let message = document.getElementById("user-message").value;
     if (message != '') {
-        alert("Message sent: " + message);
+        alert("Message sent: " + message.value);
     } else {
         alert("Please enter a message before sending.");
     }
-    if (birthDate != '') {
-        alert("Birth date is: " + birthDate);
-    } else {
-        alert("Please enter your birth date.");
-    }
-    if (gender != '') {
-        alert("Gender is: " + gender);
-    } else {
-        alert("Please select your gender.");
-    }
-    if (username != '') {
-        alert("Username is: " + username);
-    } else {
-        alert("Please enter your username.");
-    }
-    if (email != '') {
-        alert("Email is: " + email);
-    } else {
-        alert("Please enter your email.");
-    }
 
-
+}
