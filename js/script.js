868d6391aaa4fc19368b7574f1c04b4feb55fccc
Azri Kuncoro Jati <geozray@gmail.com>
2025-08-16 10:28:52 +0700

    update form styles and enhance message validation in JavaScript

--- js/script.js
+++ js/script.js
@@ -11,10 +11,31 @@ function welcomeSpeech() {
 }
 
 function sendMessage() {
-    let message = document.getElementById("user-message").value;
+    let message = document.getElementById("user-message").value};
     if (message != '') {
         alert("Message sent: " + message);
     } else {
         alert("Please enter a message before sending.");
     }
-}
+    if (birthDate != '') {
+        alert("Birth date is: " + birthDate);
+    } else {
+        alert("Please enter your birth date.");
+    }
+    if (gender != '') {
+        alert("Gender is: " + gender);
+    } else {
+        alert("Please select your gender.");
+    }
+    if (username != '') {
+        alert("Username is: " + username);
+    } else {
+        alert("Please enter your username.");
+    }
+    if (email != '') {
+        alert("Email is: " + email);
+    } else {
+        alert("Please enter your email.");
+    }
+
+
